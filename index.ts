dbc8ba86954638c3d2aa11cb27a1e37c8d5bc21a
George Kartavenko <socialimp@mail.ru>
2026-06-24 11:01:54 +0300

    :feat: module 1 is completed

--- index.ts
+++ index.ts
@@ -1,2 +1,11 @@
-const message: string = "Hello, TypeScript!";
-console.log(message);
+// const message: string = "Hello, TypeScript!";
+// console.log(message);
+
+function greet(name: string, age: number, isAdmin: boolean = false): string {
+    const status = isAdmin ? `I am is admin` : `I am not admin`
+
+    return `Hi, my name is ${name} and i am ${age} years old. ${status}`
+
+}
+
+console.log(greet("Alex", 24, false));
